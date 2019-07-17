992f69fa124907504bcd0815b26a6f04df6b914a
Alex Krupnik <krupnik_a@ukr.net>
2019-07-17 23:58:02 +0300

    test case complite

--- config/web.php
+++ config/web.php
@@ -1,5 +1,11 @@
 <?php
 return [
-  'id'=>'school',
-  'basePath'=>dirname(__DIR__,1)
+    'id'         => 'school',
+    'basePath'   => dirname(__DIR__, 1),
+    'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ]
+    ]
 ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,10 +1,49 @@
+<?php
+
+use yii\bootstrap\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+
+/**
+ * @var string $content
+ */
+$this->beginPage()
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title></title>
+    <?php $this->head(); ?>
 </head>
 <body>
-<?=$content?>
+<?php
+$this->beginBody();
+NavBar::begin([
+    'brandLabel' => 'VideoSchool',
+    'brandUrl'   => Yii::$app->homeUrl,
+    'options'    => [
+        'class' => 'navbar navbar-dark bg-info sticky-top'
+    ]
+]);
+$menu = [
+    ['label' => 'Join', 'url' => ['/site/join']],
+    ['label' => 'Login', 'url' => ['/site/login']]
+
+];
+echo Nav::widget([
+    'options' => [
+        'class' => 'navbar-nav navbar-right'
+    ],
+    'items'   => $menu
+]);
+NavBar::end();
+echo Html::tag('div', $content, ['class' => 'container-fluid']);
+$this->endBody();
+?>
+
 </body>
 </html>
+<?php
+$this->endPage();
+?>
 
